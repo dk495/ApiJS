a6e1f9b5e533b6fc48a661f5f009fe548791890d
dk495 <55878882+dk495@users.noreply.github.com>
2023-12-14 17:36:20 +0500

    upload

---
+++ astro_ring.js
@@ -0,0 +1,68 @@
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+   
+           
+
+
+ const postData = {
+        
+	CID: document.getElementById('caller_id').value,	
+	zipcode:document.getElementById('zip_code').value
+	
+  	  };
+    
+            const url = 'https://corsproxy.io/?https://rtb.ringba.com/v1/production/8704f47d296e4d41aaa5a36e8b3a2877.json';
+    
+            fetch(url, {
+                method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            
+        },
+        body: JSON.stringify(postData)
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                } else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
