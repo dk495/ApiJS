ea103f1b688aa45b3c6c0ad5cf87f01474e91da7
dk495 <55878882+dk495@users.noreply.github.com>
2024-02-02 23:50:42 +0500

    upload

---
+++ nexon_td_mva.js
@@ -0,0 +1,73 @@
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const phone_home = '+1' + document.getElementById('caller_id').value;
+            const formData = new FormData();
+            formData.append('lead_token', '0a89a3b7fb4248e58484de07a860446f');
+formData.append('traffic_source_id', '99280');
+            formData.append('first_name', document.getElementById('first_name').value);
+            formData.append('last_name', document.getElementById('last_name').value);
+            formData.append('caller_id', phone_home);
+            formData.append('email', document.getElementById('email').value);
+
+formData.append('zip', document.getElementById('zipcode').value);
+formData.append('state', document.getElementById('state').value);
+formData.append('incident_date', document.getElementById('incident_date').value);
+formData.append('were_you_injured', document.getElementById('were_you_injured').value);
+formData.append('person_at_fault', document.getElementById('person_at_fault').value);
+formData.append('have_attorney', document.getElementById('have_attorney').value);
+formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').value);
+
+
+            
+           
+    
+            const url = 'https://adolicious-llc.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
+    
+            fetch(url, {
+                method: 'POST'
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                } else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
