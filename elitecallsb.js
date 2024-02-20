fd9d3b8338ab4bc29e677a1eb81ef97e3f4c92cf
dk495 <55878882+dk495@users.noreply.github.com>
2024-02-20 23:21:34 +0500

    upload

---
+++ elitecallsb.js
@@ -0,0 +1,67 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      const phone_home = '+1' + document.getElementById('caller_id').value;
+      const formData = new FormData();
+formData.append('lead_token', '24f5d74cc5184fd9921db0109a123c47');
+	formData.append('traffic_source_id', '11729');  
+
+      formData.append('caller_id', phone_home);
+
+     
+
+      formData.append('tcpa_opt_in', document.getElementById('tcpa_opt_in').value);
+	  
+   
+
+ 
+
+      const url = 'https://elite-calls-com.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
+
+      fetch(url, {
+        method: 'POST'
+      })
+     .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                } else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
