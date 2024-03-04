021a1622d29e04c9b3d8ab314540b2786fadb477
dk495 <55878882+dk495@users.noreply.github.com>
2024-03-05 00:19:08 +0500

    Update and rename trackdrivenewapiJS.rtf to trackdrivenewapi.js

---
+++ trackdrivenewapi.js
@@ -0,0 +1,71 @@
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const phone_home = '+1' + document.getElementById('caller_id').value;
+            const formData = new FormData();
+            formData.append('lead_token', '951953f512034bcc9feb1721847d6df1');
+formData.append('traffic_source_id', '10717');
+           formData.append('first_name', document.getElementById('first_name').value);
+formData.append('last_name', document.getElementById('last_name').value);
+formData.append('caller_id', document.getElementById('caller_id').value);
+
+formData.append('state', document.getElementById('state').value);
+formData.append('zip', document.getElementById('zip').value);
+formData.append('dob_mm', document.getElementById('dob_mm').value);
+formData.append('dob_dd', document.getElementById('dob_dd’).value);
+formData.append('dob_yyyy', document.getElementById('dob_yyyy’).value);
+
+formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').value);
+
+            
+           
+    
+            const url = 'https://elite-calls-com.trackdrive.com/api/v1/leads?’ + new URLSearchParams(formData).toString();
+    
+            fetch(url, {
+                method: 'POST'
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                } else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
