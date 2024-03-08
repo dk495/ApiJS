cb919f3c9484a27dc841a3e73f532c50e611b01f
dk495 <55878882+dk495@users.noreply.github.com>
2024-03-09 02:39:59 +0500

    Update salespoint_pingpost.js

--- salespoint_pingpost.js
+++ salespoint_pingpost.js
@@ -1,7 +1,10 @@
 function pingAPI() {
+    // Disable submit button
+    document.getElementById('submitBtn').disabled = true;
+
     // Your API URL
     var Url = "https://corsproxy.io/?https://tracker.salespoint.ai/ping?pid=AF931e95e3b3054b55a018cb389a7f639a&cid=CAce309013251447b3b99ce8f0c68445cf&caller_id=";
-    var caller_id = '+1' + document.getElementById('caller_id').value;
+    var caller_id = document.getElementById('caller_id').value;
     var state = document.getElementById('state').value;
     var apiUrl = Url + caller_id + '&state=' + state;
 
@@ -11,14 +14,20 @@ function pingAPI() {
         .then(data => {
             // Check if ping was successful
             if (data.success === true && data.status === 'rejected') {
-                // Display response as it is
+                // Enable submit button
+                document.getElementById('submitBtn').disabled = false;
+
+                // Display response as danger alert
                 document.getElementById("apiResponse").innerHTML = "Ping Success But Rejected" + JSON.stringify(data);
-                document.getElementById("apiResponse").classList.add("alert-info");
-                document.getElementById("apiResponse").classList.remove("alert-danger");
+                document.getElementById("apiResponse").classList.add("alert-danger");
+                document.getElementById("apiResponse").classList.remove("alert-info");
             } else if (data.success === true && data.status === 'accepted') {
                 // If status is accepted, call postPingId
                 postPingId();
             } else {
+                // Enable submit button
+                document.getElementById('submitBtn').disabled = false;
+
                 // Display error message
                 document.getElementById("apiResponse").innerHTML = "Ping ok but. Error: " + data.errors.join(", ");
                 document.getElementById("apiResponse").classList.add("alert-danger");
@@ -26,6 +35,9 @@ function pingAPI() {
             }
         })
         .catch(error => {
+            // Enable submit button
+            document.getElementById('submitBtn').disabled = false;
+
             // Display an error message in the Bootstrap alert
             document.getElementById("apiResponse").innerHTML = "Error fetching data from the API.";
             document.getElementById("apiResponse").classList.add("alert-danger");
@@ -37,13 +49,13 @@ function postPingId() {
     const postData = {
                
         pid:'AF931e95e3b3054b55a018cb389a7f639a',
-	cid:'CAce309013251447b3b99ce8f0c68445cf',
-	caller_id: '+1' + document.getElementById('caller_id').value,
-	first_name: document.getElementById('first_name').value,
-	last_name: document.getElementById('last_name').value,
-	state: document.getElementById('state').value,
-	zip: document.getElementById('zip').value,
-	city: document.getElementById('city').value
+        cid:'CAce309013251447b3b99ce8f0c68445cf',
+        caller_id: document.getElementById('caller_id').value,
+        first_name: document.getElementById('first_name').value,
+        last_name: document.getElementById('last_name').value,
+        state: document.getElementById('state').value,
+        zip: document.getElementById('zip').value,
+        city: document.getElementById('city').value
     };
 
     const url = 'https://corsproxy.io/?https://tracker.salespoint.ai/post';
@@ -58,8 +70,12 @@ function postPingId() {
     .then(response => {
         // Handle response based on status
         if (response.status === 200 || response.status === 201) {
-// Reset form fields
-                document.getElementById('leadForm').reset();
+            // Reset form fields
+            document.getElementById('leadForm').reset();
+
+            // Enable submit button
+            document.getElementById('submitBtn').disabled = false;
+
             response.text().then(responseBody => {
                 const successAlert = `
                     <div class="alert alert-success" role="alert">
@@ -69,6 +85,9 @@ function postPingId() {
                 document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
             });
         } else if (response.status === 422) {
+            // Enable submit button
+            document.getElementById('submitBtn').disabled = false;
+
             response.json().then(data => {
                 const errorAlert = `
                     <div class="alert alert-danger" role="alert">
@@ -78,6 +97,9 @@ function postPingId() {
                 document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
             });
         } else {
+            // Enable submit button
+            document.getElementById('submitBtn').disabled = false;
+
             response.text().then(responseBody => {
                 const errorAlert = `
                     <div class="alert alert-danger" role="alert">
