d972b8579456c416871572b2ced8a6e540c985b2
dk495 <55878882+dk495@users.noreply.github.com>
2024-03-28 00:27:26 +0500

    upload

---
+++ Ringba_ApiPostingnewBAKER.js
@@ -0,0 +1,47 @@
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const phone_home = '+1' + document.getElementById('caller_id').value;
+            const formData = new FormData();
+            formData.append('callerid', phone_home);
+            formData.append('zipcode', document.getElementById('zipcode').value);
+            formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').value);
+            
+	
+
+            
+           
+    
+            const url = 'https://display.ringba.com/enrich/2375007444746110423?' + new URLSearchParams(formData).toString();
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else if (xhr.status === 0) {
+            // If status is 0 (CORS error), consider the form submitted
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else {
+            const errorAlert = `
+              <div class="alert alert-danger" role="alert">
+                Form submission failed. Please try again.
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', errorAlert);
+          }
+        }
+      };
+      xhr.send(formData);
+    });
