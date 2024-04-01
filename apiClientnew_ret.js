65c1005ee2fbb19386b8f2420a8987e9f7dc6de1
dk495 <55878882+dk495@users.noreply.github.com>
2024-04-01 21:25:10 +0500

    upload

---
+++ apiClientnew_ret.js
@@ -0,0 +1,78 @@
+   
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+document.getElementById('submitBtn').disabled = true;
+const phone_home = '+1' + document.getElementById('caller_id').value;
+    const formData = new FormData();
+  formData.append('key', '6eb0a568-caba-45e5-b8a9-374cfa43627c');
+       formData.append('first_name', document.getElementById('first_name').value);
+    formData.append('last_name', document.getElementById('last_name').value);
+    formData.append('caller_number', phone_home);
+    formData.append('caller_state', document.getElementById('state').value);
+       formData.append('email', document.getElementById('email').value);
+    formData.append('date_of_birth', document.getElementById('dob').value);
+       formData.append('caller_city', document.getElementById('city').value);
+    
+
+
+
+
+
+            
+           
+    
+            const url = 'https://retreaverdata.com/data_writing?' + new URLSearchParams(formData).toString();
+    
+            fetch(url, {
+                method: 'POST'
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+		document.getElementById('submitBtn').disabled = false;
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+                    });
+                } 
+else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
