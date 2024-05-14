3d71c49f0edc9bf14985e1fd55f943e4d2a63bb3
dk495 <55878882+dk495@users.noreply.github.com>
2024-05-14 20:45:32 +0500

    upload

---
+++ BLA_phoneChecker.js
@@ -0,0 +1,57 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      const phone_home =  document.getElementById('caller_id').value;
+      const formData = new FormData();
+
+      formData.append('phone', phone_home);
+	
+    
+      formData.append('key', 'hG36daxHzmhBZeGXMhth');
+
+ 
+
+      const url = 'https://api.blacklistalliance.net/lookup?' + new URLSearchParams(formData).toString();
+
+   
+
+fetch(url, {
+    method: 'POST'
+})
+.then(response => {
+    if (response.ok) {
+        response.json().then(data => {
+            const message = data.message;
+            let alertClass, alertMessage;
+
+            if (message === "Good") {
+                alertClass = 'alert-success';
+                alertMessage = "Form submitted successfully!";
+            } else {
+                alertClass = 'alert-danger';
+                alertMessage = "Form submission failed. Please try again.";
+            }
+
+            const responseAlert = `
+                <div class="alert ${alertClass}" role="alert">
+                    ${response.status} : ${alertMessage} Response Body: ${JSON.stringify(data)}
+                </div>`;
+
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', responseAlert);
+
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+        });
+    } else {
+        response.text().then(responseBody => {
+            const errorAlert = `
+                <div class="alert alert-danger" role="alert">
+                    Form submission failed. Please try again. Response Body: ${responseBody}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        });
+    }
+})
+.catch(error => console.error('Error:', error));
+});
