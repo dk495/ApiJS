a94f35c760e72d5aa44c420af02799e94751558e
dk495 <55878882+dk495@users.noreply.github.com>
2024-05-17 19:50:00 +0500

    Update palermogroup__td_MVA2.js

--- palermogroup__td_MVA2.js
+++ palermogroup__td_MVA2.js
@@ -11,8 +11,9 @@ formData.append('email', document.getElementById('email').value);
 formData.append('address', document.getElementById('address').value);
 api_tester(document.getElementById('caller_id').value);
 formData.append('city', document.getElementById('city').value);
+        
 formData.append('state', document.getElementById('state').value);
-formData.append('postal_code', document.getElementById('zip').value);
+formData.append('zip', document.getElementById('zip').value);
 formData.append('ip_address', document.getElementById('ip_address').value);
 formData.append('needs_attorney', document.getElementById('needs_attorney').value);
         formData.append('hospitalized_or_treated', document.getElementById('hospitalized_or_treated').value);
@@ -22,7 +23,7 @@ formData.append('needs_attorney', document.getElementById('needs_attorney').valu
 formData.append('source_url', document.getElementById('source_url').value);
 formData.append('trusted_form_cert_url', document.getElementById('trusted_form_cert_url').value);
 
-formData.append('fault', document.getElementById('person_at_fault').value);
+formData.append('person_at_fault', document.getElementById('person_at_fault').value);
 
 formData.append('injured', document.getElementById('injured').value);
 formData.append('injury_type', document.getElementById('injury_type').value);
