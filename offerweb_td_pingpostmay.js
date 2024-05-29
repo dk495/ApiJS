510297561d6a6606c678250b5d19ca49dff57686
dk495 <55878882+dk495@users.noreply.github.com>
2024-05-30 02:36:22 +0500

    Update offerweb_td_pingpostmay.js

--- offerweb_td_pingpostmay.js
+++ offerweb_td_pingpostmay.js
@@ -8,7 +8,7 @@ function pingAPI() {
 api_tester(document.getElementById('caller_id').value);
     formData.append('traffic_source_id', '514485');
     
-    formData.append('caller_id', '+1' + document.getElementById('caller_id').value);
+    formData.append('caller_id',  document.getElementById('caller_id').value);
 	formData.append('zip', document.getElementById('zip').value);
 
 const originalUrl = 'https://offerweb.trackdrive.com/api/v1/inbound_webhooks/ping/check_offerweb_buyers_medicare?' + new URLSearchParams(formData).toString();
@@ -45,7 +45,7 @@ function postPingId(pingId) {
         trackdrive_number: "+18444130320",
         traffic_source_id: "514485",
 	zip: document.getElementById('zip').value,
-        caller_id: '+1' + document.getElementById('caller_id').value,
+        caller_id:  document.getElementById('caller_id').value,
         ping_id: pingId
 	
     };
