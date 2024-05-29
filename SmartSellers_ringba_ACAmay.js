20d8d5857320c8e3d504c925ca02ec85b42a0e0a
dk495 <55878882+dk495@users.noreply.github.com>
2024-05-29 21:11:57 +0500

    upload

---
+++ SmartSellers_ringba_ACAmay.js
@@ -0,0 +1,48 @@
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const phone_home = '+1' + document.getElementById('caller_id').value;
+            const formData = new FormData();
+            formData.append('callerid', phone_home);
+            formData.append('first_name', document.getElementById('first_name').value);
+            formData.append('last_name', document.getElementById('last_name').value);
+formData.append('token', document.getElementById('token').value);
+            
+	
+
+            
+           
+    
+            const url = 'https://display.ringba.com/enrich/2441518838185985137?' + new URLSearchParams(formData).toString();
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else if (xhr.status === 0) {
+            // If status is 0 (CORS error), consider the form submitted
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else {
+            const errorAlert = `
+              <div class="alert alert-danger" role="alert">
+                Form submission failed. Please try again.
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', errorAlert);
+          }
+        }
+      };
+      xhr.send(formData);
+    });
