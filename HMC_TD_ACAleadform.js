3ee4b0382292a549cd229ddc84ecc358c2dab45a
dk495 <55878882+dk495@users.noreply.github.com>
2024-06-25 02:31:45 +0500

    Update HMC_TD_ACAleadform.js

--- HMC_TD_ACAleadform.js
+++ HMC_TD_ACAleadform.js
@@ -16,6 +16,7 @@ formData.append('email', document.getElementById('email').value);
 formData.append('phone_number', document.getElementById('caller_id').value);
 formData.append('state', document.getElementById('state').value);
 formData.append('zip', document.getElementById('zip').value);
+        formData.append('tcpa_consent', 'true');
         formData.append('household_size', document.getElementById('household_size').value);
         formData.append('yearly_income', document.getElementById('yearly_income').value);
         formData.append('pre_existing_condition', document.getElementById('pre_existing_condition').value);
