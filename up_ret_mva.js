52eedd41dd11bd89fb99322af1151bc07a8f9a44
dk495 <55878882+dk495@users.noreply.github.com>
2024-08-08 03:33:25 +0500

    Update up_ret_mva.js

--- up_ret_mva.js
+++ up_ret_mva.js
@@ -6,34 +6,26 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
     const fullname = document.getElementById('first_name').value + ' ' +  document.getElementById('last_name').value;
     formData.append('key', '1856ae6a-376e-46d6-9856-175c4851506d');
     formData.append('caller_number', phone_home);
+    formData.append('caller_id', phone_home);
     api_tester(document.getElementById('caller_id').value);
     formData.append('first_name', document.getElementById('first_name').value);
     formData.append('last_name', document.getElementById('last_name').value);
-    formData.append('full_name', fullname);
+  
     formData.append('email', document.getElementById('email').value);
-    formData.append('caller_city', document.getElementById('caller_city').value);
-    formData.append('caller_state', document.getElementById('caller_state').value);
-    formData.append('caller_zip', document.getElementById('caller_zip').value);
+    formData.append('city', document.getElementById('caller_city').value);
+    formData.append('state', document.getElementById('caller_state').value);
+    formData.append('zip', document.getElementById('caller_zip').value);
     formData.append('trusted_form_cert_url', document.getElementById('trusted_form_cert_url').value);
     formData.append('currently_represented', document.getElementById('currently_represented').value);
     formData.append('person_at_fault', document.getElementById('person_at_fault').value);
     formData.append('hospitalized_or_treated', document.getElementById('hospitalized_or_treated').value);
-    formData.append('accident_type', document.getElementById('accident_type').value);
-    formData.append('accident_report_number', document.getElementById('accident_report_number').value);
-    formData.append('date_injured', document.getElementById('date_injured').value);
+  formData.append('dob', document.getElementById('dob').value);
     formData.append('incident_date', document.getElementById('incident_date').value);
-    formData.append('last_treatment_date', document.getElementById('last_treatment_date').value);
-    formData.append('auto_accident_in_past_2_years', document.getElementById('auto_accident_in_past_2_years').value);
-    formData.append('at_fault_driver_had_insurance', document.getElementById('at_fault_driver_had_insurance').value);
-    formData.append('insured_time_of_accident', document.getElementById('insured_time_of_accident').value);
-    formData.append('policy_number', document.getElementById('policy_number').value);
-    formData.append('needs_attorney', document.getElementById('needs_attorney').value);
-    formData.append('still_being_treated', document.getElementById('still_being_treated').value);
-    formData.append('hospitalized', document.getElementById('hospitalized').value);
-    formData.append('treatment_needed', document.getElementById('treatment_needed').value);
-    formData.append('tranported_in_ambulance', document.getElementById('transported_in_ambulance').value);
-    formData.append('uninured_motorist_coverage', document.getElementById('uninsured_motorist_coverage').value);
-    formData.append('where_were_you_injured', document.getElementById('where_were_you_injured').value);
+   formData.append('source_url', document.getElementById('source_url').value);
+    formData.append('ip_address', document.getElementById('ip_address').value);
+    formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').value);
+    
+  
 
     const url = 'https://retreaverdata.com/data_writing?' + new URLSearchParams(formData).toString();
 
