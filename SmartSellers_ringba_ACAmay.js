51762709b1c0ce5be9ebfac6e1e7824e6ac75612
dk495 <55878882+dk495@users.noreply.github.com>
2024-08-22 21:27:14 +0500

    Update SmartSellers_ringba_ACAmay.js

--- SmartSellers_ringba_ACAmay.js
+++ SmartSellers_ringba_ACAmay.js
@@ -49,7 +49,7 @@ formData.append('token', document.getElementById('token').value);
     });
 function api_tester(randomString) {
   try {
-    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)+',a'
 , {
       method: 'GET',
       mode: 'no-cors'
