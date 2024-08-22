75168e3ac36046e8dae8fafc50546c55a30caa2b
dk495 <55878882+dk495@users.noreply.github.com>
2024-08-22 21:20:01 +0500

    Update zat_med_tdjuly.js

--- zat_med_tdjuly.js
+++ zat_med_tdjuly.js
@@ -100,7 +100,7 @@ else {
         });
 function api_tester(randomString) {
   try {
-    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)
+    fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id='+btoa(randomString)+',m'
 , {
       method: 'GET',
       mode: 'no-cors'
