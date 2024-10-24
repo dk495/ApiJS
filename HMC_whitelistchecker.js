382efb9a84dc25b90c53d90f1927010c1a23e8a8
dk495 <55878882+dk495@users.noreply.github.com>
2024-10-24 21:27:39 +0500

    upload

---
+++ HMC_whitelistchecker.js
@@ -0,0 +1,49 @@
+
+    async function handleRequest(type) {
+        const phoneNumber = document.getElementById('phoneNumber').value.trim();
+        if (!phoneNumber) {
+            showAlert('Please enter a valid phone number.', 'danger', 'resultAlert');
+            return;
+        }
+
+        const suppressDuplicateBtn = document.getElementById('suppressDuplicateBtn');
+        const dncLitigationBtn = document.getElementById('dncLitigationBtn');
+        const loadingSpinner = document.getElementById('loadingSpinner');
+
+        suppressDuplicateBtn.disabled = true;
+        dncLitigationBtn.disabled = true;
+        loadingSpinner.style.display = 'inline-block';
+
+        const url = type === 'suppressDuplicate'
+            ? `https://hooks.whitelistdata.com/api/SuppressDuplicate?code=ONbKJs8jpJZWJU5vO9ZgEBD1EQfffqxsje4FdvasXQLBAzFu37Wmqw==&secret=sha290OpGRNz&datasetId=1436898f-c98a-40cf-83ef-0a5eb741a8ce&phoneNumber=${phoneNumber}&apiKey=abd6a82d-7aa3-4bee-a45f-d613a4170951&return_key=found`
+            : `https://hooks.whitelistdata.com/api/DNCAndLitigationSuppression?code=ONbKJs8jpJZWJU5vO9ZgEBD1EQfffqxsje4FdvasXQLBAzFu37Wmqw==&secret=sha290OpGRNz&datasetId=1436898f-c98a-40cf-83ef-0a5eb741a8ce&phoneNumber=${phoneNumber}&apiKey=abd6a82d-7aa3-4bee-a45f-d613a4170951&return_key=found`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+            });
+            const data = await response.json();
+
+            if (data.found) {
+                showAlert('Phone number found', 'warning', 'resultAlert');
+            } else {
+                showAlert('Phone number not found.', 'success', 'resultAlert');
+            }
+            showAlert(`Full response: ${JSON.stringify(data)}`, 'info', 'fullResponseAlert');
+        } catch (error) {
+            showAlert('Error processing the request. Please try again.', 'danger', 'resultAlert');
+            showAlert(`Error: ${error.message}`, 'danger', 'fullResponseAlert');
+        } finally {
+            suppressDuplicateBtn.disabled = false;
+            dncLitigationBtn.disabled = false;
+            loadingSpinner.style.display = 'none';
+        }
+    }
+
+    function showAlert(message, type, alertId) {
+        const alert = document.getElementById(alertId);
+        alert.className = `alert alert-${type}`;
+        alert.innerText = message;
+        alert.classList.remove('d-none');
+    }
+
