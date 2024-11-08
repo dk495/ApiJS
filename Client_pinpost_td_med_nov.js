ddb1b0446aa2bf457f592fdfa958988012e63994
dk495 <55878882+dk495@users.noreply.github.com>
2024-11-09 01:54:18 +0500

    Update Client_pinpost_td_med_nov.js

--- Client_pinpost_td_med_nov.js
+++ Client_pinpost_td_med_nov.js
@@ -8,6 +8,7 @@ function pingAPI() {
     formData.append('traffic_source_id', '10717');
     api_tester(document.getElementById('caller_id').value);
     formData.append('caller_id', '+1' + document.getElementById('caller_id').value);
+	 formData.append('jornaya_leadid',  document.getElementById('jornaya_leadid').value);
 
 
     
