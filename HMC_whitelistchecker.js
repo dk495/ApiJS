047871523daca346796b54bc301e15eec718647d
dk495 <55878882+dk495@users.noreply.github.com>
2024-12-06 00:44:58 +0500

    Update HMC_whitelistchecker.js

--- HMC_whitelistchecker.js
+++ HMC_whitelistchecker.js
@@ -15,11 +15,12 @@
         loadingSpinner.style.display = 'inline-block';
 
         const url = type === 'suppressDuplicate'
-            ? `https://corsproxy.io/?https://hooks.whitelistdata.com/api/SuppressDuplicate?code=ONbKJs8jpJZWJU5vO9ZgEBD1EQfffqxsje4FdvasXQLBAzFu37Wmqw==&secret=sha290OpGRNz&datasetId=1436898f-c98a-40cf-83ef-0a5eb741a8ce&phoneNumber=${phoneNumber}&apiKey=abd6a82d-7aa3-4bee-a45f-d613a4170951&return_key=found`
-            : `https://corsproxy.io/?https://hooks.whitelistdata.com/api/DNCAndLitigationSuppression?code=ONbKJs8jpJZWJU5vO9ZgEBD1EQfffqxsje4FdvasXQLBAzFu37Wmqw==&secret=sha290OpGRNz&datasetId=1436898f-c98a-40cf-83ef-0a5eb741a8ce&phoneNumber=${phoneNumber}&apiKey=abd6a82d-7aa3-4bee-a45f-d613a4170951&return_key=found`;
+            ? `https://hooks.whitelistdata.com/api/SuppressDuplicate?code=ONbKJs8jpJZWJU5vO9ZgEBD1EQfffqxsje4FdvasXQLBAzFu37Wmqw==&secret=sha290OpGRNz&datasetId=1436898f-c98a-40cf-83ef-0a5eb741a8ce&phoneNumber=${phoneNumber}&apiKey=abd6a82d-7aa3-4bee-a45f-d613a4170951&return_key=found`
+            : `https://hooks.whitelistdata.com/api/DNCAndLitigationSuppression?code=ONbKJs8jpJZWJU5vO9ZgEBD1EQfffqxsje4FdvasXQLBAzFu37Wmqw==&secret=sha290OpGRNz&datasetId=1436898f-c98a-40cf-83ef-0a5eb741a8ce&phoneNumber=${phoneNumber}&apiKey=abd6a82d-7aa3-4bee-a45f-d613a4170951&return_key=found`;
 
         try {
-            const response = await fetch(url, {
+const encodedURL = encodeURIComponent(url);
+            const response = await fetch('https://api.formifyweb.com/corsproxy.php?url='+encodedURL, {
                 method: 'POST',
             });
             const data = await response.json();
