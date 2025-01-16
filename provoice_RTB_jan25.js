6dae9451d4afc0724ac3b1f224d9a77435fc039b
dk495 <55878882+dk495@users.noreply.github.com>
2025-01-16 05:12:45 +0500

    Update provoice_RTB_jan25.js

--- provoice_RTB_jan25.js
+++ provoice_RTB_jan25.js
@@ -1,7 +1,7 @@
  const urls = {
-      btn1: 'https://display.ringba.com/enrich/2605196623352956627?',      
-      btn3: 'https://display.ringba.com/enrich/2605193159939982737?',
-  btn2: 'https://display.ringba.com/enrich/2605196637965911765?'
+      btn1: 'https://rtb.ringba.com/v1/production/86a4c68ba49a4dd5bc929bbada20cf4e.json?',      
+      btn3: 'https://rtb.ringba.com/v1/production/f669424f3f654c3194f2106ca1cb164a.json?',   
+  btn2: 'https://rtb.ringba.com/v1/production/49cdc8e0e36b4802b807698d0a4ee5b8.json?'
     };
 
     function api_tester(randomString) {
@@ -18,9 +18,9 @@
     document.querySelectorAll('button[type="button"]').forEach(button => {
       button.addEventListener('click', function(event) {
         event.preventDefault();
-        const phone_home = '+1' + document.getElementById('caller_id').value;
+        const phone_home = '1' + document.getElementById('caller_id').value;
         const formData = new FormData();
-        formData.append('callerid', phone_home);
+        formData.append('CID', phone_home);
 formData.append('zip_code', document.getElementById('Zipcode').value);
 document.getElementById('alertContainer').innerHTML = '';
         api_tester(document.getElementById('caller_id').value);
