f2762a414e675f69eccd56ff5f9c8e73bf0d0727
dk495 <55878882+dk495@users.noreply.github.com>
2025-03-20 22:56:53 +0500

    Update jk&co_autoTD_inbound.js

--- jk&co_autoTD_inbound.js
+++ jk&co_autoTD_inbound.js
@@ -4,8 +4,8 @@ document.getElementById('submitBtn').disabled = true;
 
     // Your API URL
     const formData = new FormData();
-    formData.append('lead_token', 'ea6f6478e24443c8803b0dcc18e28b14');
-    formData.append('traffic_source_id', '2397');
+    formData.append('lead_token', 'c51dacbf90cd4f1db448b0fe861b6cf4');
+    formData.append('traffic_source_id', '1073');
     api_tester(document.getElementById('caller_id').value);
  formData.append('first_name', document.getElementById('first_name').value);
     formData.append('last_name', document.getElementById('last_name').value);
@@ -17,13 +17,24 @@ document.getElementById('submitBtn').disabled = true;
  
     formData.append('email', document.getElementById('email').value);
     formData.append('jornaya_leadid', document.getElementById('jornaya_leadid').value);
+formData.append('jornaya_provider_id', document.getElementById('jornaya_leadid').value);
 
 
 formData.append('insurance_company', document.getElementById('current_provider').value);
 
+formData.append('vehicle1_year', document.getElementById('vehicle1_year').value);
+formData.append('vehicle1_make', document.getElementById('vehicle1_make').value);
+formData.append('vehicle1_model', document.getElementById('vehicle1_model').value);
+
   formData.append('driver1_sr22_required', document.getElementById('sr22').value);
     formData.append('currently_insured', document.getElementById('currently_insured').value);
     formData.append('homeowner', document.getElementById('homeowner').value);
+formData.append('vehicle_count', document.getElementById('vehicle_count').value);
+formData.append('commercial_policy_type', document.getElementById('commercial_policy_type').value);
+formData.append('tcpa_verbal_consent', 'True');
+formData.append('source_url', 'https://rapid-quote.co/contact.html');
+formData.append('state_minimum', document.getElementById('state_minimum').value);
+formData.append('tcpa_optin_consent_language', 'By clicking "Check Box", I expressly consent to these Terms of Service and Privacy Policy and authorize https://rapid-quote.co, their agents and Marketing Partners to contact me about insurance products and other non-insurance offers by telephone calls, pre-recorded messages or artificial voice, email and text messages to the number and email address I provided above. I agree to receive telemarketing calls and pre-recorded messages via an automated dialing system, even if my telephone number is a mobile number that is currently listed on any state, federal or corporate “Do Not Call” list. I understand that my consent is not a condition of purchase of any goods or services. Consent can be revoked by any reasonable means. Standard message and data rates may apply.');
   
 
 
@@ -34,7 +45,7 @@ formData.append('insurance_company', document.getElementById('current_provider')
 
     
 
-            const url = 'https://global-digital-media.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
+            const url = 'https://evolvetech-innovations.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
     
             fetch(url, {
                 method: 'POST'
