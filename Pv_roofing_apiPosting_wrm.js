4cb3866365b11d4ff8b6c7206b87af9dad01e6ad
dk495 <55878882+dk495@users.noreply.github.com>
2025-08-25 14:46:11 +0500

    Update Pv_roofing_apiPosting_wrm.js

--- Pv_roofing_apiPosting_wrm.js
+++ Pv_roofing_apiPosting_wrm.js
@@ -14,10 +14,10 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
             roof_roofing_type: form.roof_roofing_type.value
         },
         meta: {
-            source_id: "909090",
+            source_id: form.source_id.value,
             user_agent: navigator.userAgent,
             lead_id_code: form.lead_id_code.value,
-            landing_page_url: "www.shelterfix.com",
+            landing_page_url: form.landing_page_url.value,
             tcpa_consent_text: "By completing the form, I hereby affirm my acceptance of the Terms and Conditions, CCPA , and Privacy Policy . I grant permission to Remodeling Loans, their contractors, and partners (refer to our partners list) to communicate with me through email, phone, and text messages using the provided contact number. I consent to receiving offers from these entities, even if my contacts are listed on the State and Federal Do Not Call List. I acknowledge that these marketing communications may be transmitted using an automatic telephone dialing system or pre-recorded messages. I understand that my consent is not a prerequisite for making a purchase and that I retain the right to revoke it at any time. This declaration includes compliance with the California Notice.",
             originally_created: new Date(form.originally_created.value).toISOString(),
             trusted_form_cert_url: "https://cert.trustedform.com/b8825b1810177750a475ab6a800908378136b109"
@@ -43,9 +43,9 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
             roof_roofing_type: form.roof_roofing_type.value
         },
         meta: {
-            landing_page_url: "www.shelterfix.com",
+            landing_page_url: form.landing_page_url.value,
             originally_created: new Date(form.originally_created.value).toISOString(),
-            source_id: "909090",
+            source_id: form.source_id.value,
             tcpa_consent_text: "By completing the form, I hereby affirm my acceptance of the Terms and Conditions, CCPA , and Privacy Policy . I grant permission to Remodeling Loans, their contractors, and partners (refer to our partners list) to communicate with me through email, phone, and text messages using the provided contact number. I consent to receiving offers from these entities, even if my contacts are listed on the State and Federal Do Not Call List. I acknowledge that these marketing communications may be transmitted using an automatic telephone dialing system or pre-recorded messages. I understand that my consent is not a prerequisite for making a purchase and that I retain the right to revoke it at any time. This declaration includes compliance with the California Notice.",
             trusted_form_cert_url: "https://cert.trustedform.com/b8825b1810177750a475ab6a800908378136b109",
             user_agent: navigator.userAgent,
@@ -115,4 +115,5 @@ function api_tester(randomString) {
         console.error('Error in api_tester:', error);
     }
 }
+
 
