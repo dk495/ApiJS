94d978cd1ac0ba95775248b893ec8507056ab945
Saadqazi6 <saad.qazi6@gmail.com>
2025-08-27 00:15:47 +0500

    Add files via upload

---
+++ pega2_post26aug.js
@@ -0,0 +1,73 @@
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+document.getElementById('submitBtn').disabled = true;
+             // Your API URL
+    const formData = new FormData();
+
+    api_tester(document.getElementById('caller_id').value);
+    formData.append('phone', '+1' + document.getElementById('caller_id').value);
+    formData.append('State', document.getElementById('state').value);
+    formData.append('Zip', document.getElementById('zip').value);
+
+
+
+
+
+const originalUrl = 'https://api.enrollhere.com/dialer/availability/byQueue/v54TDhakRUIsaEtoBAbw?exposeCallerId=yes&' + new URLSearchParams(formData).toString();
+const apiUrl = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(originalUrl);
+
+    fetch(apiUrl, {
+        method: 'GET'
+    })
+    .then(response => {
+        if (response.status === 200 || response.status === 201) {
+            response.json().then(responseBody => {
+                // Remove 'retreaver_payout' key from response body
+                delete responseBody.retreaver_payout;
+
+                const successAlert = `
+                    <div class="alert alert-success" role="alert">
+                        ${response.status} : Form submitted successfully! Response Body: ${JSON.stringify(responseBody)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            });
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
+        } else if (response.status === 422) {
+            response.json().then(data => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Error. Response Body: ${JSON.stringify(data)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+            });
+        } else {
+            response.text().then(responseBody => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Form submission failed. Please try again. Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
+function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
+}
