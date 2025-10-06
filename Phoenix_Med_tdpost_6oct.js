02076a1250217667ae6e2f3188e3054569fa493e
Saadqazi6 <saad.qazi6@gmail.com>
2025-10-06 21:41:08 +0500

    Update Phoenix_Med_tdpost_6oct.js

--- Phoenix_Med_tdpost_6oct.js
+++ Phoenix_Med_tdpost_6oct.js
@@ -5,7 +5,8 @@
             formData.append('lead_token', '74211a76123441db8292f9d2b0ed0004');
             formData.append('traffic_source_id', '9995');
            
-            formData.append('CID', phone_home);          
+            formData.append('CID', phone_home); 
+        formData.append('caller_id', phone_home); 
             api_tester(document.getElementById('caller_id').value);
             
            
@@ -70,6 +71,7 @@
     
   }
 }
+
 
 
 
