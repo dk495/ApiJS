e39c69cb5474d04daf5a9d06cb1f4f82dafce1fb
Saadqazi6 <saad.qazi6@gmail.com>
2025-11-06 20:54:01 +0500

    Update Pv_client2_windows_apiPosting_bid.js

--- Pv_client2_windows_apiPosting_bid.js
+++ Pv_client2_windows_apiPosting_bid.js
@@ -61,7 +61,7 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
     }
   };
 
-  const pingUrl = "https://corsproxy.io/?url=" + encodeURIComponent("https://exchange.standardinformation.io/ping?legacy=J");
+  const pingUrl = "https://corsproxy.io/?url=" + encodeURIComponent("https://exchange.standardinformation.io/ping_test?legacy=J");
 
   // Send only minimal data for ping
   fetch(pingUrl, {
@@ -78,7 +78,7 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
     if (data.status === 'success') {
       lead.auth_code = data.auth_code;
 
-      const postUrl = "https://corsproxy.io/?url=" + encodeURIComponent("https://exchange.standardinformation.io/post?legacy=J");
+      const postUrl = "https://corsproxy.io/?url=" + encodeURIComponent("https://exchange.standardinformation.io/post_test?legacy=J");
 
       // Send full lead for post
       return fetch(postUrl, {
@@ -147,6 +147,7 @@ function getRandomUserAgent() {
   const randomIndex = Math.floor(Math.random() * userAgents.length);
   return userAgents[randomIndex];
 }
+
 
 
 
