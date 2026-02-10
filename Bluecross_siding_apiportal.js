20c50c1dd078ee6eee5edc0ac2ba3a89b684bad4
Saadqazi6 <saad.qazi6@gmail.com>
2026-02-11 01:03:35 +0500

    Update Bluecross_siding_apiportal.js

--- Bluecross_siding_apiportal.js
+++ Bluecross_siding_apiportal.js
@@ -127,12 +127,12 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
                 
                 if (postResponse.status === 'ACCEPTED') {
                     alertClass = 'alert-success';
-                    message = `Lead submitted successfully! Lead ID: ${postResponse.lead_id}, Payout: $${postResponse.payout}`;
+                    message = `Lead submitted successfully! Lead ID: ${postResponse.lead_id}`;
                     document.getElementById('leadForm').reset();
                 } 
                 else if (postResponse.status === 'DUPLICATED') {
                     alertClass = 'alert-warning';
-                    message = `Duplicate lead detected. Lead ID: ${postResponse.lead_id}, Message: ${postResponse.message}`;
+                    message = `Duplicate lead detected. Lead ID: ${postResponse.lead_id};
                 }
                 else if (postResponse.status === 'ERROR') {
                     alertClass = 'alert-danger';
@@ -213,4 +213,5 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
 });
