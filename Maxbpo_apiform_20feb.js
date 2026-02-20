aa45b8cb98daa98897fb446b3f6a54901505aea2
Saadqazi6 <saad.qazi6@gmail.com>
2026-02-20 23:26:05 +0500

    Add files via upload

---
+++ Maxbpo_apiform_20feb.js
@@ -0,0 +1,70 @@
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+formData.append('key', '5666');
+formData.append('State', document.getElementById('state').value);
+
+
+
+
+
+const originalUrl = 'https://scripts.vonexta.com/covone/API/Availability.php?' + new URLSearchParams(formData).toString();
+const apiUrl = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(originalUrl);
+
+    fetch(apiUrl, {
+        method: 'GET'
+    })
+    .then(response => {
+        if (response.status === 200 || response.status === 201) {
+            response.json().then(responseBody => {
+                // Remove 'retreaver_payout' key from response body
+                delete responseBody.retreaver_payout;
+
+                const successAlert = `
+                    <div class="alert alert-success" role="alert">
+                        ${response.status} : Form submitted successfully! Response Body: ${JSON.stringify(responseBody)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            });
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
+        } else if (response.status === 422) {
+            response.json().then(data => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Error. Response Body: ${JSON.stringify(data)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+            });
+        } else {
+            response.text().then(responseBody => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Form submission failed. Please try again. Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
+function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
+}
