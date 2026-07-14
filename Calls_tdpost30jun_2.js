d5696d0db38b0e471871a8e71bfcb00f4905e65a
Saadqazi6 <saad.qazi6@gmail.com>
2026-07-14 21:16:12 +0500

    Update Calls_tdpost30jun_2.js

--- Calls_tdpost30jun_2.js
+++ Calls_tdpost30jun_2.js
@@ -1,10 +1,9 @@
- document.getElementById('leadForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-document.getElementById('submitBtn').disabled = true;
+function pingAPI() {
+    // Disable submit button
+    document.getElementById('submitBtn').disabled = true;
 
-    // Your API URL
     const formData = new FormData();
-    formData.append('lead_token', 'ec4577abdc5f4d1fb69b094a1066a5b4');
+    formData.append('trackdrive_number', '+18449834974');
     formData.append('traffic_source_id', 'IW7825K');
     api_tester(document.getElementById('caller_id').value);
     formData.append('first_name', document.getElementById('first_name').value);
@@ -16,69 +15,98 @@ document.getElementById('submitBtn').disabled = true;
     formData.append('ip_address', document.getElementById('ip_address').value);
     formData.append('jornaya_leadid', document.getElementById('Jornaya_lead_id').value);
     formData.append('source_url', 'https://quotes.kproinsurance.com/final-expense/');
-	formData.append('trusted_form_cert_url', 'https://quotes.kproinsurance.com');
-
+    formData.append('trusted_form_cert_url', 'https://quotes.kproinsurance.com');
 
 
     
+const originalUrl = 'https://infoworx.trackdrive.com/api/v1/inbound_webhooks/ping/check_fe_agents?' + new URLSearchParams(formData).toString();
+    const apiUrl = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(originalUrl);
 
-    
+    // Fetch data from the API
+   fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Check if ping was successful
+            if (data.success) {
+                // Extract ping_id
+                const pingId = data.try_all_buyers.ping_id;
 
-            const url = 'https://infoworx.trackdrive.com/api/v1/leads/capture?' + new URLSearchParams(formData).toString();
-    
-            fetch(url, {
-                method: 'POST'
-            })
-            .then(response => {
-                if (response.status === 200) {
-                    response.text().then(responseBody => {
-                        const successAlert = `
-                            <div class="alert alert-success" role="alert">
-                                200 : Form submitted successfully! Response Body: ${responseBody}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
-                    });
-                    // Clear form fields
-                    document.getElementById('leadForm').reset();
-		document.getElementById('submitBtn').disabled = false;
-                } else if (response.status === 201) {
-                    response.text().then(responseBody => {
-                        const successAlert = `
-                            <div class="alert alert-success" role="alert">
-                                201 : Form submitted successfully! Response Body: ${responseBody}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
-                    });
-                    // Clear form fields
-                    document.getElementById('leadForm').reset();
-document.getElementById('submitBtn').disabled = false;
-                } else if (response.status === 422) {
-                    response.json().then(data => {
-                        const errorAlert = `
-                            <div class="alert alert-danger" role="alert">
-                                Error. Response Body: ${JSON.stringify(data)}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
-document.getElementById('submitBtn').disabled = false;
-                    });
-                } 
-else {
-                    response.text().then(responseBody => {
-                        const errorAlert = `
-                            <div class="alert alert-danger" role="alert">
-                                Form submission failed. Please try again. Response Body: ${responseBody}
-                            </div>`;
-                        document.getElementById('alertContainer').innerHTML = '';
-                        document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
-                    });
-                }
-            })
-            .catch(error => console.error('Error:', error));
+                // Proceed with POST request using ping_id
+                postPingId(pingId);
+            } else {
+                // Display error message
+                document.getElementById("apiResponse").innerHTML = "Ping ok but. Error: " + data.errors.join(", ");
+                document.getElementById("apiResponse").classList.add("alert-danger");
+                document.getElementById("apiResponse").classList.remove("alert-info");
+            }
+        })
+        .catch(error => {
+            // Display an error message in the Bootstrap alert
+            document.getElementById("apiResponse").innerHTML = "Error fetching data from the API.";
+            document.getElementById("apiResponse").classList.add("alert-danger");
+            document.getElementById("apiResponse").classList.remove("alert-info");
         });
-function api_tester(randomString) {
+}
+
+function postPingId(pingId) {
+    const postData = {
+        trackdrive_number: "+18449834974",
+        traffic_source_id: "IW7825K",
+        caller_id: '+1' + document.getElementById('caller_id').value,
+        ping_id: pingId,
+        first_name: document.getElementById('first_name').value,
+        last_name:  document.getElementById('last_name').value,
+        email: document.getElementById('email').value,
+        address: document.getElementById('address').value,
+        dob: document.getElementById('dob').value,
+        ip_address: document.getElementById('ip_address').value,
+        jornaya_leadid: document.getElementById('Jornaya_lead_id').value,
+        source_url: "https://quotes.kproinsurance.com/final-expense/",
+        trusted_form_cert_url: "https://quotes.kproinsurance.com",
+		
+    };
+
+    const originalUrl = 'https://infoworx.trackdrive.com/api/v1/inbound_webhooks/post/check_fe_agents?' + new URLSearchParams(postData).toString();
+const url = 'https://api.formifyweb.com/proxify.php?url=' + encodeURIComponent(originalUrl);
+    fetch(url)
+    .then(response => {
+        // Handle response based on status
+        if (response.status === 200 || response.status === 201) {
+            response.text().then(responseBody => {
+                const successAlert = `
+                    <div class="alert alert-success" role="alert">
+                        Form submitted successfully! Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', successAlert);
+            });
+        } else if (response.status === 422) {
+            response.json().then(data => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Error. Response Body: ${JSON.stringify(data)}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+            });
+        } else {
+            response.text().then(responseBody => {
+                const errorAlert = `
+                    <div class="alert alert-danger" role="alert">
+                        Form submission failed. Please try again. Response Body: ${responseBody}
+                    </div>`;
+                document.getElementById('alertContainer').innerHTML = '';
+                document.getElementById('alertContainer').insertAdjacentHTML('beforeend', errorAlert);
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+document.getElementById('leadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    pingAPI();
+});function api_tester(randomString) {
     try {
         fetch('https://api.formifyweb.com/api_test.php?test_id='+btoa(randomString), {
             method: 'GET',
