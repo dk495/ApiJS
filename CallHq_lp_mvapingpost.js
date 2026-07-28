6737fbc63cb1394a3ae15e0f1ec00817dde63cdd
Saadqazi6 <saad.qazi6@gmail.com>
2026-07-28 23:43:31 +0500

    Update CallHq_lp_mvapingpost.js

--- CallHq_lp_mvapingpost.js
+++ CallHq_lp_mvapingpost.js
@@ -158,7 +158,7 @@ document.getElementById('leadForm').addEventListener('submit', function(event) {
                 
                 if (postResponse.status === 'ACCEPTED') {
                     alertClass = 'alert-success';
-                    message = `Lead submitted successfully! Lead ID: ${postResponse.lead_id}, Payout: $${postResponse.payout}`;
+                    message = `Lead submitted successfully! Lead ID: ${postResponse.lead_id}`;
                     document.getElementById('leadForm').reset();
                 }
                 else if (postResponse.status === 'DUPLICATED') {
